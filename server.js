04300ed048d8808fb208df6dee29c8a7c07cb39e
Dina Pal <dinapal.dev@gmail.com>
2023-08-19 10:21:41 +0530

    added html text

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 import express from "https://esm.sh/express?target=denonext";
 const app = express();
+
+
 app.get("/", (req, res) => {
-  res.send("Hello from Deno Deploy! working fine!!");
+  res.render('./index.html');
+
 });
 app.listen(8080);
