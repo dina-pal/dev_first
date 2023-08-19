600a2312277eda23d96af79c795818e45609d679
Dina Pal <dinapal.dev@gmail.com>
2023-08-19 10:34:59 +0530

    updated files

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 import express from "https://esm.sh/express?target=denonext";
 const app = express();
 
+app.get('/help', (req, res) =>{
+  res.send('<h1>This is Help Page </h1>');
+})
 
 app.get("/", (req, res) => {
- res.send('deno dev working fine!')
+ res.send('deno dev working fine! <a href="/help">Goto help </a>');
 
 });
 app.listen(8080);
